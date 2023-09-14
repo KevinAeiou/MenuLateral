1e392e3068eac392f76eb472b61238955dbeb400
Kevin <kevin.souzaeiou@outlook.com>
2023-09-14 13:45:47 -0400

    Correção

---
+++ app/src/main/java/com/example/menulateralgerenciadorwarspear/ui/WarspearActivityConstantes.java
@@ -0,0 +1,30 @@
+package com.example.menulateralgerenciadorwarspear.ui;
+
+public interface WarspearActivityConstantes {
+
+    String CHAVE_TRABALHO = "trabalho";
+    String CHAVE_USUARIOS = "Usuarios";
+    String CHAVE_PERSONAGEM = "Lista_personagem";
+    String CHAVE_LISTA_DESEJO = "Lista_desejo";
+    String CHAVE_LISTA_PROFISSAO = "Lista_profissoes";
+    String CHAVE_LISTA_TRABALHO = "Lista_trabalhos";
+
+    String CHAVE_NOME_RARIDADE = "nome_raridade";
+    String CHAVE_NOME_PROFISSAO = "nome_profissao";
+    String CHAVE_NOME_TRABALHO = "nome_trabalho";
+    String CHAVE_NOME_PERSONAGEM = "nome_personagem";
+    String CHAVE_POSICAO = "posicao";
+    String CHAVE_CONFIRMA_CADASTRO = "confirma_cadastro";
+    String CHAVE_TITULO_TRABALHO = "Trabalhos";
+    String CHAVE_TITULO_PROFISSAO = "Profissões";
+    String CHAVE_TITULO_RARIDADE = "Raridades";
+    String CHAVE_TITULO_CONFIRMA = "Confirma";
+    String CHAVE_TITULO_RECUPERA_SENHA = "Recupera senha";
+    String CHAVE_TITULO_PERSONAGEM = "Personagem";
+    String CHAVE_TITULO_ALTERA_STATUS_TRABALHO = "Altera estado";
+    String CHAVE_TITULO_NOVO_TRABALHO="Novo trabalho";
+    String TAG_ACTIVITY = "Estado da Activity";
+    int CODIGO_REQUISICAO_INSERE_TRABALHO = 1;
+    int CODIGO_REQUISICAO_ALTERA_TRABALHO = 2;
+    int POSICAO_INVALIDA = -1;
+}
--- app/src/main/java/com/example/menulateralgerenciadorwarspear/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/menulateralgerenciadorwarspear/ui/home/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.menulateralgerenciadorwarspear.ui.home;
 
-import android.app.ProgressDialog;
+import static com.example.menulateralgerenciadorwarspear.ui.WarspearActivityConstantes.CHAVE_PERSONAGEM;
+import static com.example.menulateralgerenciadorwarspear.ui.WarspearActivityConstantes.CHAVE_USUARIOS;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,37 +11,99 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.menulateralgerenciadorwarspear.R;
 import com.example.menulateralgerenciadorwarspear.databinding.FragmentHomeBinding;
 import com.example.menulateralgerenciadorwarspear.model.Personagem;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private ListaPersonagemAdapter personagemAdapter;
     private List<Personagem> personagens;
-    private ProgressDialog progressDialog;
     private FirebaseAuth minhaAutenticacao;
+    private FirebaseDatabase database;
     private DatabaseReference minhaReferencia;
     private RecyclerView recyclerView;
+    private String usuarioId;
     private String[] mensagens={"Carregando dados...","Erro de conexão..."};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ListaPersonagemAdapter homeViewModel =
-                new ViewModelProvider(this).get(ListaPersonagemAdapter.class);
+        //binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        minhaAutenticacao=FirebaseAuth.getInstance();
+        usuarioId= Objects.requireNonNull(minhaAutenticacao.getCurrentUser()).getUid();
+        database=FirebaseDatabase.getInstance();
+        minhaReferencia=database.getReference(CHAVE_USUARIOS);
+        recyclerView= view.findViewById(R.id.listaPersonagensRecyclerView);
+
+        pegaTodosPersonagens();
+        //recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        personagemAdapter=new ListaPersonagemAdapter(personagens,getActivity());
+        recyclerView.setAdapter(personagemAdapter);
+        //configuraRecyclerView(todosPersonagens);
+
+        //ListaPersonagemAdapter homeViewModel =
+        //        new ViewModelProvider(this).get(ListaPersonagemAdapter.class);
+
 
         //final TextView textView = binding.textHome;
         //homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return view;
+    }
+
+    private void atualizaListaPersonagem() {
+        pegaTodosPersonagens();
+        configuraRecyclerView(personagens);
+    }
+
+    private void configuraRecyclerView(List<Personagem> todosPersonagens) {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        //Collections.sort(todosPersonagens, (p1, p2) -> p1.getNome().compareToIgnoreCase(p2.getNome()));
+        configuraAdapter(todosPersonagens,recyclerView);
+    }
+
+    private void configuraAdapter(List<Personagem> todosPersonagens, RecyclerView recyclerView) {
+        personagemAdapter=new ListaPersonagemAdapter(todosPersonagens,getActivity());
+        recyclerView.setAdapter(personagemAdapter);
+    }
+
+    private void pegaTodosPersonagens() {
+        personagens=new ArrayList<>();
+        minhaReferencia.child(usuarioId).child(CHAVE_PERSONAGEM).
+                addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        personagens.clear();
+                        for (DataSnapshot dn:snapshot.getChildren()){
+                            Personagem personagem = dn.getValue(Personagem.class);
+                            personagens.add(personagem);
+                        }
+                        personagemAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     @Override
